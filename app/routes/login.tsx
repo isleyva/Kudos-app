082dd1e404afe1a816f70dc2b4ec0f6f3b9bd6b7
Ivan Sanchez Leyva <deepman42@protonmail.com>
2023-02-17 11:49:23 +0100

    layout

---
+++ app/routes/login.tsx
@@ -0,0 +1,13 @@
+import { Layout } from "~/components/Layout"
+
+
+export default function login() {
+    return (
+        <Layout>
+            <div className="h-full flex justify-center items-center flex-col gap-y-4 ">
+                <h2>Welcome to Kudos!</h2>
+            </div>
+
+        </Layout>
+    );
+}
