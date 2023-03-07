b5b2c33ab8b90b368141aa7e815a5355c3549012
Ivan Sanchez Leyva <deepman42@protonmail.com>
2023-03-07 13:07:05 +0100

    setting in the tsx

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -3,14 +3,15 @@ import { FormField } from "~/components/Form-field";
 import { useState } from "react";
 import { ActionFunction, json } from "@remix-run/node";
 import { validateEmail, validateName, validatePassword } from "~/utils/validators.server";
+import { login, register } from "~/utils/auth.server";
 
 export const action: ActionFunction = async ({ request }) => {
     const form = await request.formData()
     const action = form.get("_action")
     const email = form.get("email")
     const password = form.get("password")
-    const firstName = form.get("firstName")
-    const lastName = form.get("lastName")
+    let firstName = form.get("firstName")
+    let lastName = form.get("lastName")
 
     if (
         typeof action !== "string" ||
@@ -27,6 +28,34 @@ export const action: ActionFunction = async ({ request }) => {
         )
     ) {
         return json({ error: "Invalid form data", form: action }), { status: 400 }
+    }
+    const errors = {
+        email: validateEmail(email),
+        password: validatePassword(password),
+        ...(action === 'register'
+            ? {
+                firstName: validateName((firstName as string) || ''),
+                lastName: validateName((lastName as string) || ''),
+            }
+            : {}),
+    }
+    if (Object.values(errors).some(Boolean))
+        return json({ errors, fields: { email, password, firstName, lastName }, form: action }, { status: 400 })
+
+    switch (action) {
+        case "login": {
+            return await login({
+                email,
+                password,
+            })
+        }
+        case "register": {
+            firstName = firstName as string
+            lastName = lastName as string
+            return await register({ email, password, firstName, lastName })
+        }
+        default:
+            return json({ error: 'Invalid form data' }, { status: 400 })
     }
 }
 
