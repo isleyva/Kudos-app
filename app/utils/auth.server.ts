11dcac24a9418a1499afe2a101147eaff1ed9c8b
Ivan Sanchez Leyva <deepman42@protonmail.com>
2023-03-07 11:01:33 +0100

    create cookie

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -1,8 +1,25 @@
-import { json } from "@remix-run/node";
+import { json, createCookieSessionStorage } from "@remix-run/node";
 import { prisma } from "./prisma.server";
 import type { RegisterForm, LoginForm } from "./types.server";
 import { createUser } from "./user.server";
 import bcrypt from "bcryptjs";
+
+const sessionSecret = process.env.SESSION_SECRET;
+if (!sessionSecret) {
+  throw new Error("SESSION_SECRET must be set");
+}
+
+const storage = createCookieSessionStorage({
+  cookie: {
+    name: "kudos-session",
+    secure: process.env.NODE_ENV === "production",
+    secrets: [sessionSecret],
+    sameSite: "lax",
+    path: "/",
+    maxAge: 60 * 60 * 24 * 30,
+    httpOnly: true,
+  },
+});
 
 export const register = async (form: RegisterForm) => {
   const exists = await prisma.user.count({ where: { email: form.email } });
