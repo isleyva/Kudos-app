e9e5e971861319a2396fb79103f99051a9ecef0d
Ivan Sanchez Leyva <deepman42@protonmail.com>
2023-03-07 10:34:31 +0100

    register and login controllers

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -1,10 +1,33 @@
 import { json } from "@remix-run/node";
 import { prisma } from "./prisma.server";
-import type { RegisterForm } from "./types.server";
+import type { RegisterForm, LoginForm } from "./types.server";
+import { createUser } from "./user.server";
+import bcrypt from "bcryptjs";
 
 export const register = async (form: RegisterForm) => {
   const exists = await prisma.user.count({ where: { email: form.email } });
   if (exists) {
     return json({ error: "User already exists" }, { status: 400 });
   }
+  const newUser = await createUser(form);
+
+  if (!newUser) {
+    return json(
+      {
+        error: "Something went wrong creating new user",
+        fields: { email: form.email, password: form.password },
+      },
+      { status: 400 }
+    );
+  }
+  return null;
+};
+
+export const login = async (form: LoginForm) => {
+  const user = await prisma.user.findUnique({ where: { email: form.email } });
+
+  if (!user || !(await bcrypt.compare(form.password, form.password))) {
+    return json({ error: "Incorrect login" }, { status: 400 });
+  }
+  return null;
 };
